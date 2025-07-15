153dd8ff861e891d88fae3ccbdcfc526586a505f
witbore <witbore@gmail.com>
2025-07-15 20:22:31 +0300

    Implement group merging via graph building and BFS-based traversal
    I implemented following:
    - Merge groups if their lines share a common value at the same column index;
    - Printing the number of the resulting clusters.
    
    Merging was done via graph building and BFS grouping, both O(n^2) in the worst case.
    
    The code is still unoptimized, and the overall structure needs further polishing.

--- src/main/java/org/lng/Main.java
+++ src/main/java/org/lng/Main.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 record Pair(Integer position, String number) {
 }
@@ -24,12 +21,44 @@ public class Main {
                     if (number.length() < 3 || !number.matches("\"\\d+\"")) {
                         continue;
                     }
-                    map.computeIfAbsent(new Pair(pos, number), pair -> new ArrayList<>()).add(line);
+                    map.computeIfAbsent(new Pair(pos, number), k -> new ArrayList<>()).add(line);
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(map.size());
+
+        Map<String, Set<String>> adjacencyList = new HashMap<>();
+        for(Map.Entry<Pair, List<String>> entry : map.entrySet()) {
+            List<String> lines = entry.getValue();
+            for(String line : lines) {
+                adjacencyList.computeIfAbsent(line, k -> new HashSet<>()).addAll(lines);
+            }
+        }
+        List<List<String>> result = new ArrayList<>();
+        Set<String> visited = new HashSet<>();
+
+        for(String line : adjacencyList.keySet()) {
+            if(visited.contains(line)) {
+                continue;
+            }
+            Deque<String> queue = new ArrayDeque<>();
+            List<String> group = new ArrayList<>();
+            queue.add(line);
+
+            while(!queue.isEmpty()) {
+                String current = queue.pop();
+                if(!visited.add(current)) {
+                    continue;
+                }
+                group.add(current);
+                for (String neighbor : adjacencyList.getOrDefault(current, Collections.emptySet())) {
+                    if (!visited.contains(neighbor)) {
+                        queue.add(neighbor);
+                    }
+                }            }
+            result.add(group);
+        }
+        System.out.println(result.size());
     }
 }
