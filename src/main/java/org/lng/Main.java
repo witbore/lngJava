1d07461c531a723b86e307a24c65de35a6a8425b
witbore <witbore@gmail.com>
2025-07-15 18:30:02 +0300

    Create first draft of grouping logic via `(position, value)` mapping strategy
    First approximation looks like this:
    - Parse all lines;
    - Gather all lines with the same pair `(position, value)`;
    - Merge groups if their lines share a common value at the same column index;
    - Count the number of the resulting clusters.
    
    I've realised the raw algorithm for initial grouping.
    The issues with my current solution:
    - I need to iterate over all mapped pairs and compare associated lines for intersections, which results in `O(N^2)` merging behavior.
    - I store all original lines as-is, which is likely memory-inefficient.
    - Doesn't support CLI args yet, hardcoded file path.
    
    This version is a base sketch — not optimized but outlines the general approach.

--- src/main/java/org/lng/Main.java
+++ src/main/java/org/lng/Main.java
@@ -1,7 +1,35 @@
 package org.lng;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+record Pair(Integer position, String number) {
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        File file = new File("src/main/resources/lng-4.txt");
+        Map<Pair, List<String>> map = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            for (String line = br.readLine(); line != null; line = br.readLine()) {
+                String[] split = line.split(";");
+                for (int pos = 0; pos < split.length; pos++) {
+                    String number = split[pos];
+                    if (number.length() < 3 || !number.matches("\"\\d+\"")) {
+                        continue;
+                    }
+                    map.computeIfAbsent(new Pair(pos, number), pair -> new ArrayList<>()).add(line);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(map.size());
     }
 }
